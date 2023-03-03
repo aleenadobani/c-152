c5fa49a077f5d9608fc2858ea6da3d7ae34300ec
aleenadobani <98019373+aleenadobani@users.noreply.github.com>
2023-03-03 16:12:23 +0530

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 27 16:43:46 2023
+
+@author: User
+"""
+
+from tkinter import *
+root=Tk()
+root.title("multidimensional Arrays")
+
+root.geometry("500x500")
+label = Label(root)
+
+array_1d = ["John","James","Thomson"]
+print(array_1d[0])
+
+array_2d = [["John","A"],["James","B"],["Thomson","c"]]
+print(array_2d[0][1])
+
+array_3d = [[["John","A","Excellent"],["James","B","very good"],["Thomson","c","good"]]]
+print(array_3d[0][1][2])
+
+def report():
+    label["text"] = array_3d[0][1][0] + " got grade " + array_3d[0][1][1] +" and he is doing "+ array_3d[0][1][2]
+   
+btn = Button(root,text= "show report", command = report)
+btn.place(relx = 0.5,rely = 0.5, anchor = CENTER)
+
+label.place(relx=0.5,rely=0.6,anchor = CENTER)
+
+root.mainloop()
